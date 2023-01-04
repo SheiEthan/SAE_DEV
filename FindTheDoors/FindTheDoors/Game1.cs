58974eff958937ae7df97f4aad660bbddb1e0eaa
Sheiethan <ethan.tillier@etu.univ-smb.fr>
2023-01-04 09:27:32 +0100

    Map

--- FindTheDoors/FindTheDoors/Game1.cs
+++ FindTheDoors/FindTheDoors/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.Graphics;
 
 namespace FindTheDoors
 {
@@ -8,6 +11,8 @@ namespace FindTheDoors
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
 
         public Game1()
         {
@@ -19,11 +24,17 @@ namespace FindTheDoors
         protected override void Initialize()
         {
             base.Initialize();
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            _graphics.PreferredBackBufferWidth = 400;
+            _graphics.PreferredBackBufferHeight = 400;
+            _graphics.ApplyChanges();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _tiledMap = Content.Load<TiledMap>("sable");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
         }
 
         protected override void Update(GameTime gameTime)
@@ -31,12 +42,14 @@ namespace FindTheDoors
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             base.Update(gameTime);
+            _tiledMapRenderer.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             base.Draw(gameTime);
+            _tiledMapRenderer.Draw();
         }
     }
 }
