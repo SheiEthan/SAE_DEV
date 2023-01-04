7c87c41a6dc1066d42fd1bafa86337640c74f293
PurP74 <nabih.elhoub@etu.univ-smb.fr>
2023-01-04 09:05:12 +0100

    ok

--- FindTheDoors/FindTheDoors/Content/istockphoto-678719470-612x612.tsx
+++ FindTheDoors/FindTheDoors/Content/istockphoto-678719470-612x612.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="istockphoto-678719470-612x612" tilewidth="20" tileheight="20" tilecount="600" columns="30">
- <image source="istockphoto-678719470-612x612.jpg" width="612" height="408"/>
+ <image source="sable.jpg" width="612" height="408"/>
 </tileset>
